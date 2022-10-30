ff18d5e2299c6675966e4fad260945aaafb5c6f0
Yuri-Lima <y.m.lima@hotmail.com>
2022-10-30 17:43:08 +0000

    👌 IMPROVE: Removed production and Devs Mode

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,14 @@ require("dotenv").config();
 import http from "http";
 import https from "https";
 import express, { ErrorRequestHandler, Request, Response, NextFunction, RequestHandler, RequestParamHandler, Express, Router } from "express";
-import { join } from "node:path";
 import connectToDb from "./utils/connectToDb";
 import { log, logfile } from "./utils/logger";
 import router from "./routes/index";
 import {deserializeUser} from "./middleware/deserializeUser";
 import {rules} from "./middleware/accessControlAllow";
 import {createHttpErrorHandler} from "./middleware/errorHandler";
-import {JsonToEnv, Options_Set_Env, Set_Env} from "dynamic.envs";
 import { mongoCredentials } from "./utils/connectToDb"
-import { existsSync } from "node:fs"
+
 /**
  * @shortDescription - This is the main Class of the application
  * @class App
@@ -24,18 +22,21 @@ export default class App {
     public app: Express; // express application
     public router: Router;
     public server: http.Server | https.Server; // server
-    public is_docker_setup: boolean;
     public env: {} | [];
     public port: number; // port
     public protocol: string;
     public host: string;
-    public node_env: boolean = true;
-
+    public db_credentials: mongoCredentials = {
+        mongo_uri: <string>process.env.MONGO_URI,
+        user: <string>process.env.MONGO_INITDB_ROOT_USERNAME,
+        password: <string>process.env.MONGO_INITDB_ROOT_PASSWORD,
+        db_name: <string>process.env.DB_NAME,
+        port: Number(process.env.PORT)
+    }
     /**
      * Constructor.
      */
     constructor() {
-        this.createEnvFile();
         this.setupEnv();
         this.app = express();
         this.middleware();
@@ -43,56 +44,11 @@ export default class App {
         this.server = this.createServer();
     }
 
-    private createEnvFile(): void {
-        /**
-         * @shortDescription - This function creates the .env file
-         * Only for development mode
-         * If the .env file is not present
-         * If you run the application in production mode the dist folder will have issues
-         * If you run the application in production mode on Docker the dist folder will have issues
-         */
-        // console.log(`NODE_ENV: ${existsSync(join(process.cwd(), "src", "env.json"))}`);
-        if(existsSync(join(process.cwd(), "src", "env.json")) || existsSync(join(process.cwd(), "env.json"))) {
-                const fileName = "env.json";
-                const set:Set_Env = {
-                    fileName: fileName,
-                    readFileFrom: join(process.cwd(), "src"),
-                    saveFileTo: join(process.cwd(), fileName)
-                };
-                const options:Options_Set_Env ={
-                    overWrite_Original_Env: true, // if you dont want to overwrite your original .env file, set this to true
-                    createJsonFile: true, // If you want to create a new json file, set this to true
-                    createEnvFile: true, // If you want to create a new env file, set this to true
-                    log: false // If you want to log the result, set this to true
-                }
-                const setEnv = new JsonToEnv(set, options);
-                setEnv.setEnv();
-        }
-    }
-
     private setupEnv(): void {
-        this.is_docker_setup = Boolean(process.env.PROD_IS_DOCKER_SETUP==="false"?false:true);
-        if (process.env.NODE_ENV === "production") {
-            log.info("PRODUCTION MODE");
-            if (this.is_docker_setup) {
-                console.log(`DOCKER is SETUP on PRODUCTION MODE`);
-                this.host = process.env.PROD_HOST?process.env.PROD_HOST:"localhost";
-                this.protocol = process.env.PROD_DOCKER_TLS_SSL==="true"?"https":"http";
-                this.port = this.protocol==="https"?Number(process.env.PROD_DOCKER_HTTPS_PORT):Number(process.env.PROD_DOCKER_HTTP_PORT);
-                return;
-            }
-        }
-        else if (process.env.NODE_ENV === "development") {
-            log.info("DEVELOPMENT MODE");
-            if (this.is_docker_setup) {
-                log.info(`DOCKER is SETUP on DEVELOPMENT MODE`);
-                this.host = process.env.DEV_HOST?process.env.DEV_HOST:"localhost";
-                this.protocol = process.env.DEV_DOCKER_TLS_SSL==="true"?"https":"http";
-                this.port = this.protocol==="https"?Number(process.env.DEV_DOCKER_HTTPS_PORT):Number(process.env.DEV_DOCKER_HTTP_PORT);
-                return;
-            }
-        }
-        if(process.env.DEBUG==="true" && process.env.NODE_ENV==="development") {
+        this.host = process.env.APP_HOST || "localhost";
+        this.protocol = process.env.TLS_SSL || "http";
+        this.port = Number(process.env.PORT) || Number(process.env.APP_DOCKER_HTTP_PORT) || 3000;
+        if(process.env.DEBUG==="true") {
             //LOCAL mode
             // log.info("LOCAL is SETUP");
             // this.protocol = process.env.DEV_DOCKER_TLS_SSL==="true"?"https":"http";
@@ -114,8 +70,7 @@ export default class App {
     }
 
     private createServer(): http.Server | https.Server {
-        const value = this.protocol==="https"?true:false;
-        if (value) {
+        if (this.protocol==="https") {
             const httpsOptions = {
                 key: "",
                 cert: "",
@@ -127,33 +82,24 @@ export default class App {
         return http.createServer(this.app);
     }
     public async start(): Promise<void> {
-        let credentials:mongoCredentials={ user: "", password: "", mongo_uri: "", db_name: "" };
-        if(this.node_env){
-            credentials.mongo_uri = <string>process.env.MONGO_URI;
-            credentials.user = <string>process.env.PROD_MONGO_USER;
-            credentials.password = <string>process.env.PROD_MONGO_PASSWORD;
-            credentials.port = Number(process.env.PROD_MONGO_PORT);
-            credentials.db_name = <string>process.env.PROD_MONGO_DB_NAME;
+        try {
+            const conn = await connectToDb(this.db_credentials);
+            this.server.listen(this.port, () => {
+                log.info(`Server started on port ${this.port} on protocol ${this.protocol}`);
+            }).on("error", (error: any) => {
+                log.error(`Error starting server ${error}`);
+            }).on("listening", () => {
+                log.info(`Server listening on port ${this.port} on protocol ${this.protocol}`);
+            }).on("close", () => {
+                conn?.connection.close(() => {
+                    log.info("Connection Mongoose closed");
+                });
+                log.error(`Server closed`);
+            })
+        } catch (error: any) {
+            log.error(error);
+            this.server.close();
         }
-        else{
-            credentials.mongo_uri = <string>process.env.MONGO_URI;
-            credentials.user = <string>process.env.DEV_MONGO_USER;
-            credentials.password = <string>process.env.DEV_MONGO_PASSWORD;
-            credentials.port = Number(process.env.DEV_MONGO_PORT);
-            credentials.db_name = <string>process.env.DEV_MONGO_DB_NAME;
-        }
-        await connectToDb(credentials);
-        this.server.listen(this.port, () => {
-            log.info(`Server started on port ${this.port} on protocol ${this.protocol}`);
-        }).on("error", (error: any) => {
-            log.error(`Error starting server ${error}`);
-            logfile.info(`Error starting server ${error}`);
-        }).on("listening", () => {
-            log.info(`Server listening on port ${this.port} on protocol ${this.protocol}`);
-        }).on("close", () => {
-            log.error(`Server closed`);
-            logfile.error(`Server closed`);
-        })
     }
 }
 
--- src/utils/connectToDb.ts
+++ src/utils/connectToDb.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose,{Mongoose, Connection} from 'mongoose';
 import config from 'config';
 import { log, logfile } from './logger';
 
@@ -12,68 +12,38 @@ export type mongoCredentials ={
 
 async  function connectToDb(cred: mongoCredentials) {    
     try {
-        if (process.env.NODE_ENV === 'development') {
-            if(process.env.DEV_IS_DOCKER_SETUP === 'true') {
-                const conn = await mongoose.connect(cred.mongo_uri, {
-                    appName: process.env.APP_NAME,
-                    dbName: cred.db_name,
-                    autoCreate: true,// create the database if it does not exist
-                    auth: {
-                        username: cred.user,
-                        password: cred.password
-                    },
-                    authMechanism: "DEFAULT",// "DEFAULT", "SCRAM-SHA-1", "MONGODB-CR", "PLAIN"
-                    autoIndex: true,// disable automatic indexing 11000 duplicate key error using unique index
-                    keepAlive: true,
-                    serverApi: "1",//This is to make sure that the server is the latest version.
-                    keepAliveInitialDelay: 300000,//300000 = 5 minutes
-                    heartbeatFrequencyMS: 10000,//This is the frequency of the heartbeat.
-                    minHeartbeatFrequencyMS: 5000,//This is the minimum heartbeat frequency.
-                    tls: false,//This is to enable TLS.
-                    enableUtf8Validation: true,//This is to enable UTF-8 validation.
-                    zlibCompressionLevel: 8//This is to set the compression level.
-                });
+            const conn = await mongoose.connect(cred.mongo_uri, {
+                appName: process.env.NAME,
+                dbName: cred.db_name,
+                autoCreate: true,// create the database if it does not exist
+                auth: {
+                    username: cred.user,
+                    password: cred.password
+                },
+                authMechanism: "DEFAULT",// "DEFAULT", "SCRAM-SHA-1", "MONGODB-CR", "PLAIN"
+                autoIndex: true,// disable automatic indexing 11000 duplicate key error using unique index
+                keepAlive: true,
+                serverApi: "1",//This is to make sure that the server is the latest version.
+                keepAliveInitialDelay: 300000,//300000 = 5 minutes
+                heartbeatFrequencyMS: 10000,//This is the frequency of the heartbeat.
+                minHeartbeatFrequencyMS: 5000,//This is the minimum heartbeat frequency.
+                tls: false,//This is to enable TLS.
+                enableUtf8Validation: true,//This is to enable UTF-8 validation.
+                zlibCompressionLevel: 8//This is to set the compression level.
+            });
             
             log.info(`Connected to ${conn.STATES.connected} external DB, Colections: ${conn.modelNames()}`);
             log.info(`Disconnected to ${conn.STATES.disconnected} external DB`);
-            logfile.info(`Connected to external ${cred.mongo_uri}`);
-            }
-        }
-        else if (process.env.NODE_ENV === 'production') {
-                const conn = await mongoose.connect(cred.mongo_uri, {
-                    appName: process.env.APP_NAME,
-                    dbName: cred.db_name,
-                    autoCreate: true,// create the database if it does not exist
-                    auth: {
-                        username: cred.user,
-                        password: cred.password
-                    },
-                    authMechanism: "DEFAULT",// "DEFAULT", "SCRAM-SHA-1", "MONGODB-CR", "PLAIN"
-                    autoIndex: true,// disable automatic indexing 11000 duplicate key error using unique index
-                    keepAlive: true,
-                    serverApi: "1",//This is to make sure that the server is the latest version.
-                    keepAliveInitialDelay: 300000,//300000 = 5 minutes
-                    heartbeatFrequencyMS: 10000,//This is the frequency of the heartbeat.
-                    minHeartbeatFrequencyMS: 5000,//This is the minimum heartbeat frequency.
-                    tls: false,//This is to enable TLS.
-                    enableUtf8Validation: true,//This is to enable UTF-8 validation.
-                    zlibCompressionLevel: 8//This is to set the compression level.
-                });
-            
-            log.info(`Connected to ${conn.STATES.connected} external DB, Colections: ${conn.modelNames()}`);
-            log.info(`Disconnected to ${conn.STATES.disconnected} external DB`);
-            logfile.info(`Connected to external ${cred.mongo_uri}`);
-        }
+            return conn;
     } catch (error:any) {
         /**
          * Gracefully handle mongoose connection errors
          * @see https://mongoosejs.com/docs/guide.html#error-handling
          */
-        log.error(cred.mongo_uri);
-        log.error(cred.db_name);
-        log.error(`Error connecting to ${error.stack}`);
-        logfile.error(`Error connecting to ${error}`);
-    // process.exit(1);
+        // log.error(`Error connecting to ${error.stack}`);
+        // logfile.error(`Error connecting to ${error}`);
+        throw error;
+        // process.exit(1);
     }
 }
 export default connectToDb;
